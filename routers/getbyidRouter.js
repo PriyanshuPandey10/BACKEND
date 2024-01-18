ba3d7558dd73f47ca89cefed76d1bdc1c1512be0
PriyanshuPandey10 <pandey.priyanshu10@gmail.com>
2024-01-18 21:56:57 +0530

    added routers

--- routers/getbyidRouter.js
+++ routers/getbyidRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 router.get('/add', (req,res) => {
-    res.send("Add post from post");
+    res.send("GetById Router");
 });
 module.exports = router;
--- routers/updateRouter.js
+++ routers/updateRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 router.get('/add', (req,res) => {
-    res.send("Add post from post");
+    res.send("Update Router");
 });
 module.exports = router;
